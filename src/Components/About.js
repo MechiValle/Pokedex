2b0fda59e4af4d7d400e9c658343602320190f06
MechiValle <mercedesvalle17@gmail.com>
2022-03-08 12:50:36 -0300

    Changes on About page

--- src/Components/About.js
+++ src/Components/About.js
@@ -5,6 +5,7 @@ const About = () => {
     return(
             <div className="about">
                 Pokedex created with Create React App.
+                <p>API provided by <a href="https://pokeapi.co/" target="_blank">PokeAPI</a>.</p>
             </div>
     );
 };
